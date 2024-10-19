9467dd62c4445e3fbbc9259e0db8e8952bfc3d3d
leesw1945 <67689184+leesw1945@users.noreply.github.com>
2024-10-20 02:30:50 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.43 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,31 +1,29 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
-        int min = Integer.MAX_VALUE;
 
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]<min){
-                min = arr[i];
+        if (arr.length == 1) {
+            arr[0] = -1;
+            return arr;
+        }
+
+        int minIndex = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[minIndex] > arr[i]) {
+                minIndex = i; 
             }
         }
 
-        List<Integer> list = new ArrayList<>();
-        for (int i : arr) {
-            if(i != min){
-                list.add(i);
-            }
-        }
-        //list.remove(min);
+        int[] result = new int[arr.length - 1];
 
-        if(list.size() == 0){
-            int[] answer = {-1};
-            return answer;
-        }else{
-            int[] answer = new int[list.size()];
-            for(int i=0; i<list.size(); i++){
-                answer[i] = list.get(i);
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i != minIndex) {
+                result[index++] = arr[i];
             }
-            return answer;
         }
+
+        return result;
     }
 }
