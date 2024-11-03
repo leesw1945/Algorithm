e3101757764c1fb019caf350e732bfeeb4086618
leesw1945 <67689184+leesw1945@users.noreply.github.com>
2024-11-03 21:40:07 +0900

    [level 1] Title: 시저 암호, Time: 37.25 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        for(int i = 0; i < s.length(); i++ ) {
+            int num = Integer.valueOf(s.charAt(i));
+            if(num >= 65 && num <= 90) {
+                num += n;
+                if (num > 90) {
+                    num-=26;
+                }
+                answer += (char)num;
+            } else if (num >= 97 && num <= 122) {
+                num += n;
+                if (num > 122) {
+                    num-=26;
+                }
+                answer += (char)num;
+            } else if(num == 32) {
+                answer += " ";
+            }
+        }
+
+        return answer;
+    }
+}
