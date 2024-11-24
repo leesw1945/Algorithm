2a4740f3e0a990dc46abec18dee5e7b2835d2584
leesw1945 <67689184+leesw1945@users.noreply.github.com>
2024-11-25 00:03:14 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.45 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String s) { 
+        StringBuilder sb = new StringBuilder();
+        int size = s.length();
+        boolean first = true;
+        for(int i = 0; i < size; i++){
+            char cur = s.charAt(i);
+            // 이번 문자가 공백이라면?
+            if(cur == ' '){
+                sb.append(' ');
+                first = true;
+                continue;
+            }
+            // JadenCase 첫 문자 X
+            if(!first){
+                sb.append(Character.toString(cur).toLowerCase());
+                continue;
+            }
+            // JandenCase 첫 문자 O
+            sb.append(Character.toString(cur).toUpperCase());
+            first = false;
+        }
+        return sb.toString();
+    }
+}
