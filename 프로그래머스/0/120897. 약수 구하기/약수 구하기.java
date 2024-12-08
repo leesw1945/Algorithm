020f129f0090b4577988e21164d91eafec05f544
leesw1945 <67689184+leesw1945@users.noreply.github.com>
2024-12-08 22:30:22 +0900

    [level 0] Title: 약수 구하기, Time: 0.17 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+
+            if (n % i == 0) {
+
+                list.add(i);
+            }
+        }
+        
+        int[] result = new int[list.size()];
+        
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        
+        return result;
+    }
+}
